0810affadd399051aebfb4d8bd2d60dad62ff7d3
zhenhao goh <kenneth.gzhao@outlook.com>
2024-09-06 17:42:14 +0800

    first commit

---
+++ src/components/minikit-provider.js/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MiniKit } from "@worldcoin/minikit-js";
+import propTypes from 'prop-types';
+
+export default class MiniKitProvider extends React.Component {
+  componentDidMount() {
+    MiniKit.install();
+  }
+
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
+
+MiniKitProvider.propTypes = {
+  children: propTypes.object.children,
+};
