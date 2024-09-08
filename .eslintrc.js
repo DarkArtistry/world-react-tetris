d82b2508eda57ba3ced3baa1feef4e3148966368
amirsaeed671 <amir.saeed.ali92@gmail.com>
2024-09-08 13:37:19 +0500

    fix: format of .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/jsx-filename-extension": [2, { extensions: ['.js','.jsx'] }],
-        "func-names": [0],
-        "new-cap": [2, { newIsCap: true ,capIsNew: true, capIsNewExceptions: ['List', 'Map']}],
-        "linebreak-style": [0],
-        "quotes": [0],
-        "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
-    },
-    "env": {
-        "browser": true,
-        "es6": true
-    }
+  extends: "airbnb",
+  plugins: [
+    "react",
+  ],
+  rules: {
+    "react/jsx-filename-extension": [2, { extensions: ['.js', '.jsx'] }],
+    "func-names": [0],
+    "new-cap": [2, { newIsCap: true, capIsNew: true, capIsNewExceptions: ['List', 'Map'] }],
+    "linebreak-style": [0],
+    quotes: [0],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
 };
