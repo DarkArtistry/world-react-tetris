12b1bd9164989dad8b3ce9adb6993e1364cbc1e9
abidali671 <abid.saeed.ali92@gmail.com>
2024-10-10 12:35:04 +0500

    fix dragging

--- src/components/draggable/index.js
+++ src/components/draggable/index.js
@@ -88,9 +88,7 @@ class Draggable extends React.Component {
   };
 
   handleMouseClick = () => {
-    const { startX, startY, endX, endY, dragDirection } = this.state;
-
-    console.log(startX, startY, endX, endY, dragDirection);
+    const { endX, endY, dragDirection } = this.state;
 
     if (endX === 0 && endY === 0 && dragDirection === null) {
       this.onDragUp();
@@ -142,33 +140,15 @@ class Draggable extends React.Component {
     });
   };
 
-  componentDidMount() {
-    // Mouse events
-    document.addEventListener("mousemove", this.handleMove);
-    document.addEventListener("mouseup", this.handleEnd);
-
-    // Touch events
-    document.addEventListener("touchmove", this.handleMove);
-    document.addEventListener("touchend", this.handleEnd);
-  }
-
-  componentWillUnmount() {
-    // Mouse events
-    document.removeEventListener("mousemove", this.handleMove);
-    document.removeEventListener("mouseup", this.handleEnd);
-
-    // Touch events
-    document.removeEventListener("touchmove", this.handleMove);
-    document.removeEventListener("touchend", this.handleEnd);
-  }
-
   render() {
     return (
       <div
         className={style.draggable}
-        //onMouseDown={this.handleStart} // Start drag on mouse down
-        onTouchStart={this.handleStart} // Start drag on touch start
-        // onClick={this.handleMouseClick}
+        onTouchStart={this.handleStart}
+        onMouseMove={this.handleMove}
+        onMouseUp={this.handleEnd}
+        onTouchMove={this.handleMove}
+        onTouchEnd={this.handleEnd}
       />
     );
   }
